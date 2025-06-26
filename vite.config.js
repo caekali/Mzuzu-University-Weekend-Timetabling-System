3ed56649500d0d0a3706694d28b0a75e9bbae67a
Bester Gondwe <143722159+Bester-Gondwe@users.noreply.github.com>
2025-06-26 10:23:55 +0200

    OK

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-     server: {
-        host: '0.0.0.0',
-        port: 5173, 
-        strictPort: true,
-        hmr: {
-            host: '192.168.31.33',
-        },
-    },
 });
