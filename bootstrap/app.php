ba07a378fa7a959cb3610bf503415269d7b1508c
caekali <ckalikunde@gmail.com>
2025-07-02 23:40:25 +0200

    Add trustProxies middleware configuration to application bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'profile.setup' => EnsureProfileIsSetup::class
         ]);
 
+        $middleware->trustProxies(at: '*');
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
