d3b5c176ecbebd9b30fb1513a03fc52c477d78c6
Caesar Kalikunde <114939438+caekali@users.noreply.github.com>
2025-07-02 23:11:45 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\App;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,9 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (App::environment('production')) {
-        URL::forceScheme('https');
-    }
     
     }
 }
